a3dce2bed03e228c8dd0eec269699ba63b6e2ad6
anam soomro <anamsoomroed@gmail.com>
2020-01-20 07:01:55 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,72 @@
-# Add your code here
+require 'pry'
+
+def say_hello(name)
+  "Hi #{name}!"
+end
+
+def help()
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+
+end
+
+def list(songs)
+  songs.each_with_index do |val, index|
+    puts "#{index+1}. #{val}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.strip
+  #when you do input.to_i it does 0
+  if input.to_i.to_s == input && input.to_i <= songs.length + 1
+    puts "Playing #{songs[input.to_i-1]}"
+  elsif songs.include? (input)
+    puts "Playing #{input}"
+  else 
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox()
+  puts "Goodbye"
+end
+
+def run(songs) 
+  # initialize command and do the first pass
+  puts "Please enter a command:"
+  command = gets.strip 
+  if command == "help"
+    help()
+  elsif command == "list"
+    list(songs)
+  elsif command == "play"
+    play(songs)
+  elsif command == "exit"
+    exit_jukebox()
+  else
+    puts "Invalid command"
+  end
+  # now loop until user exits
+  # damn this isnt looping
+  while !command == "exit" do
+    puts "Please enter a command:"
+    command  = gets.strip
+    if command == "help"
+      help()
+    elsif command == "list"
+      list(songs)
+    elsif command == "play"
+      play(songs)
+    elsif command == "exit"
+      exit_jukebox()
+    else
+      puts "Invalid command"
+    end
+  end
+end
+
